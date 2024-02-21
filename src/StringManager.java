3bec02d4aa5396560830b49163dec5817a49b9aa
moura1001 <gmouraneto01@gmail.com>
2024-02-21 18:25:24 -0300

    feat: iteração 2, refatoração para respeitar a restrição de no máximo 10 linhas por método

--- src/StringManager.java
+++ src/StringManager.java
@@ -4,18 +4,26 @@ import java.util.List;
 public class StringManager {
     public static List<String> converterCamelCase(String original) {
         List<String> result = new ArrayList<>(4);
+        StringManager.dividirPalavrasPorCaracterMaiusculoEAdicionarNaLista(original, result);
+        return result;
+    }
+
+    private static void dividirPalavrasPorCaracterMaiusculoEAdicionarNaLista(String original, List<String> result) {
         String element = "";
         for (int i = 0; i < original.length(); i++) {
             char c = original.charAt(i);
-            if(i > 0 && Character.isUpperCase(c)){
-                result.add(element.toLowerCase());
-                element = "" + c;
-                continue;
-            }
+            element = StringManager.adicionarNovaPalavraNaListaSeNecessario(i, c, element, result);
             element += c;
         }
         if(result.isEmpty() || !element.isEmpty())
             result.add(element.toLowerCase());
-        return result;
+    }
+
+    private static String adicionarNovaPalavraNaListaSeNecessario(int i, char c, String element, List<String> result) {
+        if(i > 0 && Character.isUpperCase(c)){
+            result.add(element.toLowerCase());
+            element = "";
+        }
+        return element;
     }
 }
